1dfef64edabc88a808c5960c5df63f83df596beb
QAKPCAof <sittipong.keaksaard@kingpower.com>
2024-06-27 10:47:45 +0700

    testApi

---
+++ tests/api-test/api.data.ts
@@ -0,0 +1,19 @@
+export const dataApi = {
+  getData: {
+    baseUrl: "https://reqres.in/api",
+    userId: 2,
+    email: "eve.holt@reqres.in",
+    password: "cityslicka",
+    page: 2,
+    per_page: 6,
+    total: 12,
+    total_pages: 2,
+  },
+  userData: {
+    id: 7,
+    email: "michael.lawson@reqres.in",
+    first_name: "Michael",
+    last_name: "Lawson",
+    avatar: "https://reqres.in/img/faces/7-image.jpg",
+  },
+};
---
+++ tests/api-test/api.test.spec.ts
@@ -0,0 +1,36 @@
+import { expect, test } from "@playwright/test";
+import { dataApi } from "./api.data.ts";
+import { VerifyResponse } from "./api.funtion.ts";
+
+const verifyResponse = new VerifyResponse();
+const dataRes = dataApi.getData;
+const dataUserList = dataApi.userData;
+
+test("should able to login via api", async ({ request }) => {
+  const resp = await request.get(`${dataRes.baseUrl}/users?page=2`);
+  const respBody = await resp.json();
+  await verifyResponse.verifyResponseDataGetUser(
+    respBody,
+    dataRes.page,
+    dataRes.per_page,
+    dataRes.total,
+    dataRes.total_pages,
+    dataUserList.id,
+    dataUserList.email,
+    dataUserList.first_name,
+    dataUserList.last_name
+  );
+});
+
+test("should login successfully", async ({ request }) => {
+  const response = await request.post(`${dataRes.baseUrl}`, {
+    data: {
+      email: dataRes.email,
+      password: dataRes.password,
+    },
+  });
+  // Add assertions based on API response
+  await verifyResponse.verifyStatusCode(response);
+  const responseData = await response.json();
+  expect(responseData).toHaveProperty("token");
+});
